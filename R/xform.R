4ded8ba466b7196519a0f18a9d04720c9e355941
Gregory Jefferis <jefferis@gmail.com>
2020-03-09 20:46:10 +0000

    memoise java init

--- R/xform.R
+++ R/xform.R
@@ -105,7 +105,7 @@ saalfeld_xform <- function(points, reg, inverse=FALSE, level=NA, stderr=FALSE, .
 
 
 ## Intention is for this to be run once e.g. by memoisation
-saalfeld_jinit <- function() {
+saalfeld_jinit <- memoise::memoise(function() {
   if(!requireNamespace('rJava', quietly = TRUE))
     stop("You must install the rJava package in order to use the (faster) rjava transform method!\n",
          "Get it from CRAN with\n",
@@ -114,7 +114,7 @@ saalfeld_jinit <- function() {
   rJava::.jinit()
   jarfile <- system.file("java/transform-helpers-0.0.1-shaded.jar", package = 'nat.h5reg')
   rJava::.jaddClassPath(jarfile)
-}
+})
 
 saalfeld_rjava_xform <- function(points, reg, inverse=FALSE, level=NA, ...) {
   saalfeld_jinit()
